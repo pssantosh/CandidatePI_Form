4bbe2fd0afc4f95067ff4781e2fb88a5add47e4f
pssantosh <ps.santosh3@gmail.com>
2017-06-12 12:23:41 +0530

    Changes for Admin

---
+++ ARC.PIForm.Web/Controllers/Admin/CandidateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ARC.PIForm.Web.Controllers.Admin
+{
+    public class CandidateController : Controller
+    {
+        // GET: Candidate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
